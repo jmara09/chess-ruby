e750d8ab468905c8c75a8a2624632b7e368313eb
Jmara <jmara.0923@gmail.com>
2024-11-16 19:09:14 +0400

    Change method name to deltas

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,29 +1,42 @@
 class Piece
   attr_reader :symbol
-  attr_accessor :coordinates, :moved
+  attr_accessor :notation, :moved, :player
 
-  def initialize(position = nil, symbol = nil)
-    @coordinates = position
+  def initialize(notation = nil, player = 1, symbol = nil)
+    @player = player
+    @notation = notation
     @symbol = symbol
     @moved = false
   end
 
-  def convert_coordinates(position = coordinates)
-    col_from_coord = position.chars.first
-    row_from_coord = position.chars.last.to_i
-    unless position.chars.length == 2 && (col_from_coord.between?('a', 'h') && row_from_coord.between?(0, 7))
+  def convert_notation(position = notation)
+    col = position.chars.first
+    row = position.chars.last.to_i
+    unless position.chars.length == 2 && (col.between?('a', 'h') && row.between?(0, 7))
       puts 'Invalid input.'
       return
     end
 
     last_row = 8
     columns = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
-    converted_row = last_row - row_from_coord
-    converted_col = columns[col_from_coord.to_sym]
+    converted_row = last_row - row
+    converted_col = columns[col.to_sym]
     [converted_row, converted_col]
   end
 
-  def available_squares(edges)
-    current_position = convert_coordinates
+  def deltas
+    raise NotImplementedError, 'Subclasses must define the method'
+  end
+
+  def available_squares(deltas)
+    current_position = convert_notation
+    squares = []
+    deltas.each do |delta|
+      square = Array.new(2, nil)
+      square[0] = delta[0] + current_position[0]
+      square[1] = delta[1] + current_position[1]
+      squares << square
+    end
+    squares
   end
 end
--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -1,17 +1,17 @@
-require_relative '../../lib/pieces/piece'
+require_relative '../../lib/pieces/load_pieces'
 
 describe Piece do
   subject(:piece) { described_class.new }
-  describe '#convert_coordinates' do
+  describe '#convert_notation' do
     context 'when a valid position is received' do
       before do
-        piece.coordinates = 'a1'
+        piece.notation = 'a1'
       end
 
       it 'converts it into an array and position from array' do
-        position = piece.coordinates
+        position = piece.notation
         position_from_array = [7, 0]
-        converted = piece.convert_coordinates(position)
+        converted = piece.convert_notation(position)
         expect(converted).to be_an(Array).and eq(position_from_array)
       end
     end
@@ -19,27 +19,27 @@ describe Piece do
     context 'when an invalid position is received' do
       context 'when the input is out of bounds' do
         before do
-          piece.coordinates = 'z9'
+          piece.notation = 'z9'
         end
 
         it 'return an error message' do
-          position = piece.coordinates
+          position = piece.notation
           error_message = 'Invalid input.'
           expect(piece).to receive(:puts).with(error_message)
-          piece.convert_coordinates(position)
+          piece.convert_notation(position)
         end
       end
 
       context 'when the input is a long string' do
         before do
-          piece.coordinates = 'Hello, world!'
+          piece.notation = 'Hello, world!'
         end
 
         it 'return an error message' do
-          position = piece.coordinates
+          position = piece.notation
           error_message = 'Invalid input.'
           expect(piece).to receive(:puts).with(error_message)
-          piece.convert_coordinates(position)
+          piece.convert_notation(position)
         end
       end
     end
@@ -47,15 +47,18 @@ describe Piece do
 
   describe '#available_squares' do
     context 'if the piece is a Pawn object' do
+      let(:pawn) { instance_double(Pawn) }
+
       before do
-        allow(piece).to receive(:convert_coordinates).and_return([6, 0])
+        allow(piece).to receive(:convert_notation).and_return([6, 0])
+        allow(pawn).to receive(:deltas).and_return([[-1, 0]])
       end
 
       it 'returns a square one row ahead' do
-        position = piece.convert_coordinates
-        square_ahead = [position[0] - 1, position[1]]
-        result = piece.available_squares
-        expect(result).to eq(square_ahead)
+        deltas = pawn.deltas
+        square_ahead = [5, 0]
+        result = piece.available_squares(deltas)
+        expect(result).to include(square_ahead)
       end
     end
   end
