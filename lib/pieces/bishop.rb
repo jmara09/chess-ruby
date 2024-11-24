8ae14980648ff2eb20fc568572b84ead97569ff5
Jmara <jmara.0923@gmail.com>
2024-11-24 19:12:29 +0300

    Override deltas in bishop class

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -7,4 +7,15 @@ class Bishop
     symbol = player == 1 ? white_bishop : black_bishop
     super(notation, player, symbol)
   end
+
+  def deltas
+    deltas = { upper_left: [], upper_right: [], lower_left: [], lower_right: [] }
+    (1..7).each do |index|
+      deltas[:upper_left] << [-index, -index]
+      deltas[:upper_right] << [-index, index]
+      deltas[:lower_left] <<  [index, -index]
+      deltas[:lower_right] << [index, index]
+    end
+    deltas
+  end
 end
--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -5,7 +5,4 @@ describe Rook do
   let(:chess_board) { ChessBoard.new.board }
   let(:own_piece) { double('rook', player: 1) }
   let(:enemy_piece) { double('rook', player: 2) }
-
-  describe '#check_square' do
-  end
 end
