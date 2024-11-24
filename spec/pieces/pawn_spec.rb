9e88f178e1f7146516d1113cf351fb104e76474c
Jmara <jmara.0923@gmail.com>
2024-11-24 10:23:24 +0300

    Edit tests for pawn

--- spec/pieces/pawn_spec.rb
+++ spec/pieces/pawn_spec.rb
@@ -12,51 +12,13 @@ describe Pawn do
     context 'if moved is false' do
       it 'returns a delta used to move two squares forward' do
         two_squares_ahead = [-2, 0]
-        result = pawn.deltas(chess_board, pawn.player)
+        result = pawn.deltas[:forward]
         expect(result).to include(two_squares_ahead)
       end
     end
-
-    context 'if the return value of #check_diagonal is not empty' do
-      context 'when there is two deltas returned' do
-        before do
-          allow(pawn).to receive(:check_diagonal).and_return([[-1, -1], [-1, 1]])
-        end
-
-        it 'will be included in the return value' do
-          left_diagonal = [-1, -1]
-          right_diagonal = [-1, 1]
-          result = pawn.deltas(chess_board, pawn.player)
-          expect(result).to include(left_diagonal).and include(right_diagonal)
-        end
-      end
-
-      context 'when only one is returned' do
-        before do
-          allow(pawn).to receive(:check_diagonal).and_return([[-1, -1], [-1, 1]])
-        end
-
-        it 'will be included in the return value' do
-          left_diagonal = [-1, -1]
-          result = pawn.deltas(chess_board, pawn.player)
-          expect(result).to include(left_diagonal)
-        end
-      end
-
-      context 'when there is no return value from #check_diagonal' do
-        before do
-          allow(pawn).to receive(:check_diagonal).and_return([])
-        end
-
-        it 'is not included in the return value' do
-          left_diagonal = [-1, -1]
-          right_diagonal = [-1, 1]
-          result = pawn.deltas(chess_board, pawn.player)
-          expect(result).not_to include(left_diagonal, right_diagonal)
-        end
-      end
-    end
   end
+
+  describe
 
   describe '#available_squares' do
     before do
@@ -65,7 +27,7 @@ describe Pawn do
     end
 
     context 'if the deltas received are [-1, 0] and [-2, 0]' do
-      it 'returns a square directly ahead and two squares ahead' do
+      xit 'returns a square directly ahead and two squares ahead' do
         deltas = [[-1, 0], [-2, 0]]
         one_square_ahead = [1, 1]
         two_squares_ahead = [0, 1]
@@ -75,7 +37,7 @@ describe Pawn do
     end
 
     context 'if there is an enemy diagonally positioned both side' do
-      it 'returns squares where the enemy piece is positioned' do
+      xit 'returns squares where the enemy piece is positioned' do
         enemy_deltas = [[-1, -1], [-1, 1]]
         upper_left = [1, 0]
         upper_right = [1, 2]
