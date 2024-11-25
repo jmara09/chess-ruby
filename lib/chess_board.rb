244cc6e5ed665ccb96f6f96b80b6a413adfd498b
Jmara <jmara.0923@gmail.com>
2024-11-25 19:55:34 +0300

    Add notations method in chess board

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -4,4 +4,19 @@ class ChessBoard
   def initialize
     @board = Array.new(8) { Array.new(8) { { symbol: ' ', notation: nil, piece: nil } } }
   end
+
+  def notations
+    row_length = @board.size
+
+    @board.each do |row|
+      col = 'a'
+
+      row.each do |square|
+        square[:notation] = col + row_length.to_s
+        col.next!
+      end
+
+      row_length -= 1
+    end
+  end
 end
