229d7f63cfe4885c9df065c4d31cc7e593d29e8a
Jmara <jmara.0923@gmail.com>
2024-11-27 19:43:28 +0300

    Transfer method convert notation to a module

---
+++ lib/convertable.rb
@@ -0,0 +1,18 @@
+module Convertable
+  def to_coord(notation)
+    return puts 'Error input. Position is nil' if notation.nil?
+
+    col = notation.chars.first
+    row = notation.chars.last.to_i
+    unless notation.chars.length == 2 && (col.between?('a', 'h') && row.between?(0, 7))
+      puts 'Invalid input.'
+      return
+    end
+
+    last_row = 8
+    columns = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
+    converted_row = last_row - row
+    converted_col = columns[col.to_sym]
+    [converted_row, converted_col]
+  end
+end
