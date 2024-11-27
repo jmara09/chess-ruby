d52211d49114e29e7d763153abcfe6ea0ce96797
Jmara <jmara.0923@gmail.com>
2024-11-27 19:49:10 +0300

    Change name of methods in piece tests

--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -4,7 +4,7 @@ describe Piece do
   subject(:piece) { described_class.new }
   let(:chess_board) { ChessBoard.new.board }
 
-  describe '#convert_notation' do
+  describe '#to_coord' do
     context 'when a valid position is received' do
       before do
         piece.notation = 'a1'
@@ -13,7 +13,7 @@ describe Piece do
       it 'converts it into an array and position from array' do
         position = piece.notation
         position_from_array = [7, 0]
-        converted = piece.convert_notation(position)
+        converted = piece.to_coord(position)
         expect(converted).to be_an(Array).and eq(position_from_array)
       end
     end
@@ -28,7 +28,7 @@ describe Piece do
           position = piece.notation
           error_message = 'Invalid input.'
           expect(piece).to receive(:puts).with(error_message)
-          piece.convert_notation(position)
+          piece.to_coord(position)
         end
       end
 
@@ -41,7 +41,7 @@ describe Piece do
           position = piece.notation
           error_message = 'Invalid input.'
           expect(piece).to receive(:puts).with(error_message)
-          piece.convert_notation(position)
+          piece.to_coord(position)
         end
       end
     end
@@ -53,13 +53,13 @@ describe Piece do
 
       before do
         chess_board[6][1][:piece] = knight
-        allow(piece).to receive(:convert_notation).and_return([7, 1])
+        allow(piece).to receive(:to_coord).and_return([7, 1])
       end
 
       it 'returns a message' do
         message = 'own piece'
         delta = [-1, 0]
-        position = piece.convert_notation
+        position = piece.to_coord
         result = piece.check_square(delta, position, chess_board).first
         expect(result).to eq(message)
       end
@@ -70,13 +70,13 @@ describe Piece do
 
       before do
         chess_board[6][1][:piece] = knight
-        allow(piece).to receive(:convert_notation).and_return([7, 1])
+        allow(piece).to receive(:to_coord).and_return([7, 1])
       end
 
       it 'returns a message and the square' do
         square_ahead = [6, 1]
         message = 'enemy piece'
-        position = piece.convert_notation
+        position = piece.to_coord
         delta = [-1, 0]
         result = piece.check_square(delta, position, chess_board)
         expect(result).to include(message, square_ahead)
@@ -85,13 +85,13 @@ describe Piece do
 
     context 'when the square is empty' do
       before do
-        allow(piece).to receive(:convert_notation).and_return([7, 0])
+        allow(piece).to receive(:to_coord).and_return([7, 0])
       end
 
       it 'returns a message the square' do
         square_ahead = [6, 0]
         message = 'empty'
-        position = piece.convert_notation
+        position = piece.to_coord
         delta = [-1, 0]
         result = piece.check_square(delta, position, chess_board)
         expect(result).to include(message, square_ahead)
@@ -105,7 +105,7 @@ describe Piece do
 
       before do
         allow(pawn).to receive(:deltas).and_return({ forward: [[-1, 0]] })
-        allow(piece).to receive(:convert_notation).and_return([6, 0])
+        allow(piece).to receive(:to_coord).and_return([6, 0])
         allow(piece).to receive(:check_square).and_return(['empty', [5, 0]])
       end
 
@@ -118,7 +118,7 @@ describe Piece do
 
       context 'when the piece is already on the last row' do
         before do
-          allow(piece).to receive(:convert_notation).and_return([0, 1])
+          allow(piece).to receive(:to_coord).and_return([0, 1])
           allow(piece).to receive(:check_square).and_return([nil, nil])
         end
 
@@ -135,7 +135,7 @@ describe Piece do
       let(:one_square_ahead) { [1, 1] }
 
       before do
-        allow(piece).to receive(:convert_notation).and_return([2, 1])
+        allow(piece).to receive(:to_coord).and_return([2, 1])
         allow(knight).to receive(:player).and_return(1)
         chess_board[one_square_ahead[0]][one_square_ahead[1]][:piece] = knight
       end
