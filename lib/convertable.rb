0224bf05028992923b3ce8147cb42c1d9e2c7331
Jmara <jmara.0923@gmail.com>
2024-12-02 19:15:38 +0300

    Change return value of to_coord if input invalid

--- lib/convertable.rb
+++ lib/convertable.rb
@@ -1,10 +1,10 @@
 module Convertable
   def to_coord(notation)
-    return false if notation.nil?
+    return 'Invalid' if notation.nil?
 
     col = notation.chars.first
     row = notation.chars.last.to_i
-    return false unless notation.chars.length == 2 && (col.between?('a', 'h') && row.between?(0, 7))
+    return 'Invalid' unless notation.chars.length == 2 && (col.between?('a', 'h') && row.between?(0, 7))
 
     last_row = 8
     columns = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
