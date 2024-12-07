7736e70fbe095a43b17c9f4781966f9dff9a9adf
Jmara <jmara.0923@gmail.com>
2024-12-07 19:27:52 +0400

    Change return value to nil if invalid

--- lib/position_converter.rb
+++ lib/position_converter.rb
@@ -1,10 +1,10 @@
 module PositionConverter
   def to_coord(notation)
-    return 'Invalid' if notation.nil?
+    return nil if notation.nil?
 
     col = notation.chars.first
     row = notation.chars.last.to_i
-    return 'Invalid' unless notation.chars.length == 2 && (col.between?('a', 'h') && row.between?(0, 7))
+    return nil unless notation.chars.length == 2 && (col.between?('a', 'h') && row.between?(0, 7))
 
     last_row = 8
     columns = { a: 0, b: 1, c: 2, d: 3, e: 4, f: 5, g: 6, h: 7 }
