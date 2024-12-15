785ea776c77118e1551b08e6c4d0cdc69c1b89ad
Jmara <jmara.0923@gmail.com>
2024-12-15 18:04:12 +0300

    Remove check and checkmate from king
    Transferred the method to chess game class

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -11,23 +11,4 @@ class King < Piece
   def deltas
     vertical(1).merge(horizontal(1)).merge(diagonal(1))
   end
-
-  def check?(enemy_pieces, board)
-    return false if enemy_pieces.empty?
-
-    enemy_pieces.any? do |piece|
-      piece.available_squares(piece.deltas, board).include?(coord)
-    end
-  end
-
-  def check_mate(enemy_pieces, board)
-    return false if enemy_pieces.empty?
-
-    all_movements = enemy_pieces.flat_map { |piece| piece.available_squares(piece.deltas, board) }
-    king_moves = available_squares(deltas, board)
-
-    return check?(enemy_pieces, board) if king_moves.empty?
-
-    king_moves.all? { |square| all_movements.include?(square) }
-  end
 end
