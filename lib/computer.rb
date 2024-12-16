cb785e4feb91142ffe2d776fd4c191568c2bd55d
Jmara <jmara.0923@gmail.com>
2024-12-17 00:29:37 +0300

    Remove debugging lines

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,7 +1,8 @@
 require_relative 'chess_board'
 
 class Computer
-  attr_reader :active_pieces, :captured, :color, :king
+  attr_accessor :active_pieces
+  attr_reader :captured, :color, :king
 
   def initialize(color, king, pieces)
     @color = color
@@ -13,7 +14,7 @@ class Computer
   def random_piece(board)
     shuffled_pieces = @active_pieces.shuffle
     piece = shuffled_pieces.find do |p|
-      !p.available_squares(p.deltas, board).empty?
+      !p.available_squares(p.deltas, board, color).empty?
     end
     return nil unless piece
 
@@ -24,8 +25,8 @@ class Computer
     piece = random_piece(board)
     return puts 'No available piece' if piece.nil?
 
-    legal_moves = piece.available_squares(piece.deltas, board)
-    raise "No legal moves available for piece: #{piece.inspect}" if legal_moves.empty?
+    legal_moves = piece.available_squares(piece.deltas, board, color)
+    puts "No legal moves available for piece: #{piece.inspect}" if legal_moves.empty?
 
     random_coord = legal_moves.sample
     random_square = board[random_coord[0]][random_coord[1]]
@@ -38,6 +39,7 @@ class Computer
     piece.moved = true if piece.is_a?(Pawn)
     board[random_coord[0]][random_coord[1]] = piece
     board[piece.coord[0]][piece.coord[1]] = ''
+
     piece.coord = random_coord
   end
 end
