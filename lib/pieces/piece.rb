bdd78ef3ec28f90ff6088b63dcf238c964fc5b48
Jmara <jmara.0923@gmail.com>
2024-12-17 12:44:31 +0300

    Refactor availables squares

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -16,46 +16,29 @@ class Piece
     raise NotImplementedError, 'Subclasses must define the method'
   end
 
-  def check_square(delta, current_position, board, color)
-    result = [nil, nil]
-    square = [
-      delta[0] + current_position[0],
-      delta[1] + current_position[1]
-    ]
-
-    return result unless square[0].between?(0, 7) &&
-                         square[1].between?(0, 7)
-
-    piece = board[square[0]][square[1]]
-
-    if piece == ''
-      result = ['empty', square]
-    elsif piece.color != color
-      result = ['enemy piece', square]
-    elsif piece.color == color
-      result[0] = 'own piece'
-    end
-
-    result
-  end
-
-  def available_squares(delta_group, board, own_color)
-    current_position = coord
+  def available_squares(board)
     squares = []
-    delta_group.each do |direction, deltas|
-      deltas.each do |delta|
-        square_element, square = check_square(delta, current_position, board, own_color)
-        next if square_element.nil?
+    deltas.each_value do |offsets|
+      offsets.each do |delta|
+        square = [
+          delta[0] + coord[0],
+          delta[1] + coord[1]
+        ]
 
-        if direction == :around && square_element != 'own piece'
-          squares << square
-          next
+        next unless square[0].between?(0, 7) &&
+                    square[1].between?(0, 7)
+
+        piece = board[square[0]][square[1]]
+
+        if piece.color == color
+          next if piece.is_a?(Knight)
+
+          break
         end
 
-        break if square_element == 'own piece'
-
         squares << square
-        break if square_element == 'enemy piece'
+
+        break if piece.color != color && !piece.is_a?(Knight)
       end
     end
     squares
